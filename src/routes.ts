45d63cac002410b198c9a3857f167b880550f172
mark on github <markongithub@users.noreply.github.com>
2023-08-18 08:00:12 -0400

    Fix all remaining tsc errors

--- src/routes.ts
+++ src/routes.ts
@@ -1,86 +1,76 @@
 import express from "express";
 const app = express()
-import { importGtfs } from 'gtfs';
+import { openDb } from 'gtfs';
 import { readFile } from 'fs/promises';
 import * as MyCode from './backend_original.js';
 const path = require('path');
 const config = require('./config')
 const morgan = require('morgan')
+const db = openDb(config);
 
-try {
-	  await importGtfs(config);
-} catch (error) {
-	  console.error(error);
-}
 
 var publicPath = path.join(__dirname, "public")
 // Delete this log when Glitch is stable
 console.log("Static files are stored in ", publicPath);
 app.use(express.static(publicPath))
 app.use('/pikaday',
-        express.static('node_modules/pikaday'))
+  express.static('node_modules/pikaday'))
 
 app.use(morgan('combined'))
 
 app.set('view engine', 'pug')
 app.get('/', function (req, res) { res.redirect('/trainsunthingy'); })
 app.get('/trainsunthingy', function (req, res) {
-  MyCode.getAgencyKeysP().then(result => res.render('index', {
+  return res.render('index', {
     title: 'Train sun thingy',
     mapsKey: config.mapsKey,
-    agencyKeys: result }))
+    agencyKeys: MyCode.getAgencyKeys()
+  });
 })
 
 app.get('/dates', function (req, res) {
-   MyCode.getDates8601P(req.query.agencyKey).then(result => res.send(result));
+  res.send(MyCode.getDates8601(db, req.query.agencyKey))
 })
 
 app.get('/stops', function (req, res) {
-   MyCode.getSourceStopsP(req.query.agencyKey).then(result => res.send(result));
+  res.send(MyCode.getSourceStops(db, req.query.agencyKey))
 })
 
 app.get('/trips', function (req, res) {
-   MyCode.getDeparturesForStopAndDateAjaxP(
-     req.query.agencyKey, req.query.sourceStop, req.query.date).then(
-       result => res.send(result));
+  res.send(MyCode.getDeparturesForStopAndDateAjax(
+    db, req.query.agencyKey, req.query.sourceStop, req.query.date))
 })
 
 app.get('/destinations', function (req, res) {
-   MyCode.getSubsequentStopsP(
-     req.query.agencyKey, req.query.sourceStop, req.query.trip).then(
-       result => res.send(result));
+  res.send(MyCode.getSubsequentStops(
+    req.query.agencyKey, req.query.sourceStop, req.query.trip))
 })
 
 //agencyKey, tripID, startDate, fromStop, toStop)
 app.get('/verdict', function (req, res) {
-   MyCode.getYearVerdictAjaxP(
-     req.query.agencyKey, req.query.trip, req.query.date, req.query.sourceStop,
-     req.query.destStop).then(result => {
-     // console.log("Sending result: " + result);
-     res.send(result);
-   });
+  res.send(MyCode.getYearVerdictAjax(
+    req.query.agencyKey, req.query.trip, req.query.date, req.query.sourceStop,
+    req.query.destStop))
 })
 
 app.get('/geojson', function (req, res) {
-   MyCode.getGeoJSONAjaxP(
-     req.query.agencyKey, req.query.trip, req.query.date, req.query.sourceStop,
-     req.query.destStop).then(result => {
-     res.send(result);
-   });
+  res.send(MyCode.getGeoJSONAjax(
+    req.query.agencyKey, req.query.trip, req.query.date, req.query.sourceStop,
+    req.query.destStop))
 })
 
 app.get('/freshness', function (req, res) {
-   MyCode.dataFreshnessP().then(result => res.send(result));
+  res.send(MyCode.dataFreshness())
 })
 
 if (config.ssl) {
   require('greenlock-express').create({
-  server: config.letsEncryptServer
-, email: config.letsEncryptContact
-, agreeTos: true
-, approveDomains: config.letsEncryptDomains
-, debug: true
-, app: app
+    server: config.letsEncryptServer
+    , email: config.letsEncryptContact
+    , agreeTos: true
+    , approveDomains: config.letsEncryptDomains
+    , debug: true
+    , app: app
   }).listen(80, 443);
 }
 else {
